b3b27080c42c24889def9533c5e4440335614374
Йордан Йорданов <dani_yordani@abv.bg>
2021-03-25 10:40:51 +0200

    application user

---
+++ Termin/Termin/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Termin.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First name")]
+        [StringLength(20,MinimumLength =2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Middle name")]
+        [StringLength(20,MinimumLength =2)]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name ="Last name")]
+        [StringLength(20,MinimumLength =2)]
+        public string LasName { get; set; }
+    }
+}
