5d0a6910ec287f2c19721276ad01c9e3b0b32b53
Gergana <dani_yordani@abv.bg>
2021-04-21 12:01:21 +0300

    Office work 2
    Added post form for the filters

--- Termin/Termin/Pages/Tests.cshtml.cs
+++ Termin/Termin/Pages/Tests.cshtml.cs
@@ -9,8 +9,21 @@ namespace Termin.Pages
 {
     public class TestsModel : PageModel
     {
+        public string Acctive { get; set; }
+        public string Passed { get; set; }
+        public string Current { get; set; }
+        [BindProperty]
+        public string Name { get; set; }
+
+
         public void OnGet()
         {
+            
+        }
+
+        public void OnPostFilterForm()
+        {
+            ;
         }
     }
 }
