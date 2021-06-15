f9018eb3065c3ec08ec13c8cd57a116666e1e06c
Gergana <dani_yordani@abv.bg>
2021-06-15 17:16:01 +0300

    View Component working

--- Termin/Termin/Startup.cs
+++ Termin/Termin/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -16,6 +17,7 @@ using Termin.AutoMapperProfiles;
 using Termin.Data;
 using Termin.Data.Repositories;
 using Termin.Hubs;
+using Termin.Middlewares;
 using Termin.Models;
 
 namespace Termin
@@ -39,6 +41,30 @@ namespace Termin
             services.AddDefaultIdentity<ApplicationUser>(/*options => options.SignIn.RequireConfirmedAccount = true*/)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddLocalization(options =>
+            {
+                options.ResourcesPath = "Resources";
+            });
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.SetDefaultCulture("en-Us");
+                options.AddSupportedUICultures("en-US", "de-DE", "ja-JP");
+                options.FallBackToParentUICultures = true;
+
+                var requestProvider = options.RequestCultureProviders.OfType<AcceptLanguageHeaderRequestCultureProvider>().First();
+                options
+                .RequestCultureProviders
+                .Remove(requestProvider);
+            });
+
+            services
+                .AddRazorPages()
+                .AddViewLocalization();
+
+            services.AddScoped<RequestLocalizationCookiesMiddleware>();
+
 
             services.AddTransient<UserRepository>();
             services.AddTransient<RoleRepository>();
@@ -84,6 +110,11 @@ namespace Termin
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseRequestLocalization();
+
+            // will remember to write the cookie 
+            app.UseRequestLocalizationCookies();
 
             app.UseRouting();
 
